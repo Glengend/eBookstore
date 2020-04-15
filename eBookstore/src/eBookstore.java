d0f16b432383b2fdb27638f492cd17b0761f735e
Glen Dorning <50457485+Glengend@users.noreply.github.com>
2020-04-15 15:16:25 +0200

    Add files via upload

---
+++ eBookstore/src/eBookstore.java
@@ -0,0 +1,11 @@
+import com.mysql.cj.protocol.Resultset;
+import java.lang.*;
+import java.sql.*;
+
+public class eBookstore {
+
+    public static void main(String[] args)
+    {
+        StartupMenu.MenuOptions();
+    }
+}
